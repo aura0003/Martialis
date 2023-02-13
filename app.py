45381457db08d1be772f06a87af6361b7d8bb0d1
Aura <coolboy1384@gmail.com>
2023-02-13 13:19:19 -0500

    update on EDSR

--- app.py
+++ app.py
@@ -18,6 +18,33 @@ img = cv2.imdecode(arr, -1) # 'Load it as it is'
 # Upscale and blur image
 img = cv2.pyrUp(img)
 
+# EDSR Super Resolution Upscaling
+sr = cv2.dnn_superres.DnnSuperResImpl_create()
+ 
+path = "EDSR_x4.pb"
+ 
+sr.readModel(path)
+ 
+sr.setModel("edsr",4)
+ 
+result = sr.upsample(img)
+ 
+# Resized image
+resized = cv2.resize(img,dsize=None,fx=4,fy=4)
+ 
+plt.figure(figsize=(12,8))
+plt.subplot(1,3,1)
+# Original image
+plt.imshow(img[:,:,::-1])
+plt.subplot(1,3,2)
+# SR upscaled
+plt.imshow(result[:,:,::-1])
+plt.subplot(1,3,3)
+# OpenCV upscaled
+plt.imshow(resized[:,:,::-1])
+plt.show()
+
+
 # Convert to grayscale and apply Gaussian filtering
 gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 gray, img_bin = cv2.threshold(gray,128,255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
