f5f3cdd7af75ab580399db5a90b67e4be5d8b3a9
Aura <coolboy1384@gmail.com>
2023-02-13 16:28:07 -0500

    Completed EDSR Integration
    honestly if I do say so myself, lowkey proud also big W

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 from urllib.request import Request, urlopen
 import cv2
 
-# Read image from which text needs to be extracted
+# Read image from URL
 req = Request(
     url = 'https://cdn.discordapp.com/attachments/1074390415618359459/1074514708213792899/image.png',
     headers={'User-Agent': 'Mozilla/5.0'}
@@ -14,36 +14,20 @@ req = Request(
 wp = urlopen(req)
 arr = np.asarray(bytearray(wp.read()), dtype=np.uint8)
 img = cv2.imdecode(arr, -1) # 'Load it as it is'
-
-# Upscale and blur image
-img = cv2.pyrUp(img)
+  
+# Remove alpha channel
+a,b,g,r = cv2.split(img)
+img = cv2.merge((r,g,b))
 
 # EDSR Super Resolution Upscaling
 sr = cv2.dnn_superres.DnnSuperResImpl_create()
- 
 path = "EDSR_x4.pb"
- 
 sr.readModel(path)
- 
 sr.setModel("edsr",4)
- 
 result = sr.upsample(img)
- 
-# Resized image
-resized = cv2.resize(img,dsize=None,fx=4,fy=4)
- 
-plt.figure(figsize=(12,8))
-plt.subplot(1,3,1)
-# Original image
-plt.imshow(img[:,:,::-1])
-plt.subplot(1,3,2)
-# SR upscaled
-plt.imshow(result[:,:,::-1])
-plt.subplot(1,3,3)
-# OpenCV upscaled
-plt.imshow(resized[:,:,::-1])
-plt.show()
 
+# Resized image
+img = cv2.resize(img,dsize=None,fx=4,fy=4)
 
 # Convert to grayscale and apply Gaussian filtering
 gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -54,6 +38,10 @@ gray = cv2.bitwise_not(img_bin)
 kernel = np.ones((2, 1), np.uint8)
 img = cv2.erode(gray, kernel, iterations=1)
 img = cv2.dilate(img, kernel, iterations=1)
+
+# Upscale and blur image
+img = cv2.pyrUp(img)
+
 # Print extracted text
 out_below = pt.image_to_string(img)
 print("OUTPUT:", out_below)
